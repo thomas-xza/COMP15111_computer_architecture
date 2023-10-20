01210e3616dd438ff4c6160908ae9466410d1b27
x93125tp <thomas.prior@student.manchester.ac.uk>
2023-10-20 14:30:02 +0100

    Garbage commit

--- lab2/part1.s
+++ lab2/part1.s
@@ -3,6 +3,7 @@
 
 op1	DEFB	"Operand 1:  \0"
 op2	DEFB	"Operand 2:  \0"
+op3	DEFB	"Result of Addition:  \0"
 
 	ALIGN
 
@@ -12,13 +13,16 @@ main
 
   SVC	5		    ; input a digit to R0
 
+  ADD R1, R0, #0
+
   ADR	R0, op2	; printf("Operand 2: ")
   SVC 3
 
-
   SVC	5		    ; input a digit to R0
 
-					; printf("Result of Addition: )
-					; printf(operand 1 + operand 2)
+  ADD R1, R1, R0
+	;;   ADR R0, op3			; printf("Result of Addition: )  SVC 3	;
+	
+	;;   ADR R0, op3			printf(operand 1 + operand 2)
 
   SVC 2		        ; stop the program
