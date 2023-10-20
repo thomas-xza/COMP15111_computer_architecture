850bf8be1fde9dff0ff3654aab6f0e8f0da019a5
x93125tp <thomas.prior@student.manchester.ac.uk>
2023-10-20 14:43:43 +0100

    Finally there, part 1 of lab 2 complete

--- lab2/part1.s
+++ lab2/part1.s
@@ -8,21 +8,27 @@ op3	DEFB	"Result of Addition:  \0"
 	ALIGN
 
 main
-  ADR	R0, op1	; printf("Operand 1: ")
-  SVC 3
+  ADR	R0, op1			; printf("Operand 1: ")
+  SVC 3				; output string (from r0)
 
-  SVC	5		    ; input a digit to R0
+  SVC 5			    	; input int to R0
 
-  ADD R1, R0, #0
 
-  ADR	R0, op2	; printf("Operand 2: ")
-  SVC 3
 
-  SVC	5		    ; input a digit to R0
 
-  ADD R1, R1, R0
-	;;   ADR R0, op3			; printf("Result of Addition: )  SVC 3	;
+  ADD R1, R0, #0   		; add r0 contents to zero, store in r1
+
+  ADR R0, op2			; printf("Operand 2: ")
+  SVC 3				; output string (from r0)
+
+  SVC 5			    	; input int to R0
+
+  ADD R1, R1, R0		; add r1 to r0, store in r1
+
+  ADR R0, op3			; move PC to location of op3
+  SVC 3				; output int		printf(operand 1 + operand 2)
+
+  ADD R0, R1, #0
+  SVC 4	
 	
-	;;   ADR R0, op3			printf(operand 1 + operand 2)
-
   SVC 2		        ; stop the program
